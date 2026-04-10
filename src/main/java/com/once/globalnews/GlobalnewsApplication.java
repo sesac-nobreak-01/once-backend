b49e5c7d137b4bdf9de0b84eebbd0250265f9573
ParkSeongGeun <phd0801@naver.com>
2026-04-10 16:38:44 +0900

    [Init] First Commit

---
+++ src/main/java/com/once/globalnews/GlobalnewsApplication.java
@@ -0,0 +1,13 @@
+package com.once.globalnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/once/globalnews/GlobalnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.once.globalnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
